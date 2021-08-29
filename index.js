05c3f1be50e322fdf1d6a1e28e6bb00668027b0f
Asif Estiak <me.asif.ae@gmail.com>
2021-08-29 16:08:49 +0600

    server file uploaded

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express()
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+
+// From DotENV File
+const port = process.env.PORT || 5555
+const dbUsername = process.env.DB_USERNAME
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
+const dbColAllCalendar = process.env.DB_COL_ALL_CALENDAR
+const dbColAllEvents = process.env.DB_COL_ALL_EVENTS
+
+const dbUri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.lq9rh.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+// Middle Ware
+app.use(cors())
+app.use(bodyParser.json())
+
+const client = new MongoClient(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  // Get Error
+  console.log('Connection error:', err);
+
+  // Access to All Calendar Collection
+  const allCalendarCollection = client.db(dbName).collection(dbColAllCalendar);
+  const allEventsCollection = client.db(dbName).collection(dbColAllEvents);
+  console.log(`Database Connected Successfully! The port is ${port}`);
+
+  // Add Calendar API
+  app.post('/addCalendar', (req, res) => {
+    const newCalendar = req.body;
+    console.log('adding new Service', newCalendar);
+    allCalendarCollection.insertOne(newCalendar)
+    .then(result => {
+      console.log('inserted count:', result.insertedCount);
+      res.send(result.insertedCount > 0)
+    })
+  })
+
+  // All Calendars List API
+  app.get('/calendars', (req, res) => {
+    allCalendarCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+  // Add Events API
+  app.post('/addEvents', (req, res) => {
+    const newEvents = req.body;
+    console.log('adding new Service', newEvents);
+    allEventsCollection.insertOne(newEvents)
+    .then(result => {
+      console.log('inserted count:', result.insertedCount);
+      res.send(result.insertedCount > 0)
+    })
+  })
+
+  // Events by Calendar ID and Date
+  app.get('/events', (req, res) => {
+    console.log(req.query.calendar, req.query.date);
+    const filter = { calendarId: req.query.calendar, eventDate: req.query.date }
+    allEventsCollection.find(filter)
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.get('/', (req, res) => {
+    res.send(`Example app listening at http://localhost:${port}`)
+  })
+
+  // perform actions on the collection object
+  // client.close();
+});
+
+
+
+
+app.listen(port)
